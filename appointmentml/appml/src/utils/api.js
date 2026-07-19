12043c185d77ce370d6a9db6e36f7a4e69ce4d2f
kameskill <johnlhoydbugayyy@gmail.com>
2026-07-19 19:18:48 +0800

    Added messages page for Admin dashboard

--- appointmentml/appml/src/utils/api.js
+++ appointmentml/appml/src/utils/api.js
@@ -46,6 +46,9 @@ export const adminApi = {
     deleteAppointment: (id) => api.delete(`/admin/appointments/${id}`),
     getAnalytics: () => api.get('/admin/analytics'),
     getContacts: () => api.get('/admin/contacts'),
+    getContacts: () => api.get('/admin/contacts'),
+    markContactRead: (id) => api.patch(`/admin/contacts/${id}/read`),
+    deleteContact: (id) => api.delete(`/admin/contacts/${id}`),
 
     // Added for the specific-user notification dropdown
     getUsers: () => api.get('/admin/users'),
